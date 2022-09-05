9ff3871ecc7fd8300cfbdc65b9e1ce25df2a7539
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-09-05 14:59:49 +0545

    Created Base

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const json5 = require("json5");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: {
+		index: "./src/index.js",
+	},
+	devtool: "inline-source-map",
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+		}),
+	],
+	optimization: {
+		runtimeChunk: "single",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+
+			{
+				test: /\.json5$/i,
+				type: "json",
+				parser: {
+					parse: json5.parse,
+				},
+			},
+		],
+	},
+};
